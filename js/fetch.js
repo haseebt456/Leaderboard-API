af40bf84817213d6d02b5398171d670d2a3d4b7b
haseebt456 <m.haseebtufail@gmail.com>
2023-12-07 21:59:09 +0500

    create separate files for js

---
+++ js/fetch.js
@@ -0,0 +1,23 @@
+const urlg = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/S83UAltkfWmJPSLpFDoC/scores/";
+
+const sendData = async (data)=>{
+
+    let options = {
+        method: "POST",
+        headers: {
+            "content-type": "Application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    let p = await fetch(urlg,options);
+    let response = await p.json();
+    return response;
+};
+
+const getData = async ()=>{
+    let p = await fetch(urlg);
+    let res = p.json();
+    return res.result;
+}
+
--- js/index.js
+++ js/index.js
@@ -1,27 +1,5 @@
-const urlg = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/S83UAltkfWmJPSLpFDoC/scores/";
 
-const sendData = async (data)=>{
 
-    let options = {
-        method: "POST",
-        headers: {
-            "content-type": "Application/json"
-        },
-        body: JSON.stringify(data)
-    }
-
-    let p = await fetch(urlg,options);
-    let response = await p.json();
-    return response;
-};
-
-const getData = async ()=>{
-    await fetch(urlg,).then((response)=>{
-        response.json();
-    }).then((DATA)=>{
-        return DATA.result;
-})
-};
 
 document.getElementById('input-data').addEventListener('submit',async (e)=>{
     e.preventDefault();
@@ -39,6 +17,10 @@ document.getElementById('input-data').addEventListener('submit',async (e)=>{
 const displayScore = async ()=>{
     const score = await getData();
     console.log(score);
+    score.forEach(element => {
+        console.log(element.user);
+        console.log(element.score);        
+    });
 }
 
 displayScore();

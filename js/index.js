288c88f1780283582687ae367888dd1285e46bf3
haseebt456 <m.haseebtufail@gmail.com>
2023-12-08 00:14:46 +0500

    merge different modules into one

--- js/index.js
+++ js/index.js
@@ -1,6 +1,45 @@
+const urlg = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IemWo1301q86738Ytc1D/scores/";
+
+const sendData = async (data)=>{
+
+    let options = {
+        method: "POST",
+        headers: {
+            "content-type": "Application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    let p = await fetch(urlg,options);
+    let response = await p.json();
+    return response;
+};
+
+const getData = async ()=>{
+    let p = await fetch(urlg);
+    let response = await p.json();
+    return response.result;
+}
+
+const scorecard = document.querySelector('.leaderboard');
+
+const render = async ()=>{
+    scorecard.innerHTML = '';
+    const data = await getData();
+    const list = data.map((item)=>{
+        const tr = document.createElement('tr');
+                const td1 = document.createElement('td');
+                const td2 = document.createElement('td');
+                td1.innerHTML = item.user;
+                td2.innerHTML = item.score;
+                tr.appendChild(td1);
+                tr.appendChild(td2);
+                scorecard.appendChild(tr);
+        })
+}
 
 
-
+/* 
 document.getElementById('input-data').addEventListener('submit',async (e)=>{
     e.preventDefault();
     let nameValue= document.querySelector('#name').value;
@@ -13,23 +52,24 @@ document.getElementById('input-data').addEventListener('submit',async (e)=>{
     let sc = await sendData(data);
     console.log(sc);
 })
-
-const displayScore = async ()=>{
-    const score = await getData();
-    console.log(score);
-    score.forEach(element => {
-        console.log(element.user);
-        console.log(element.score);        
-    });
-}
-
-displayScore();
-/* const mainFUnc = async ()=>{
-    let data = {
-        user : "haseeb",
-        score: "20"
-    };
-    let res = await sendData(data);
-    console.log(res);
-}
  */
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Add event listeners to the buttons
+    document.querySelector('#refresh').addEventListener('click', async () => { render(); });
+  
+    document.getElementById('input-data').addEventListener('submit', async (event) => {
+      event.preventDefault();
+      let nameValue= document.querySelector('#name').value;
+    let scoreValue = document.querySelector('#score').value;
+    console.log(nameValue+" is being added with score "+scoreValue);
+    let data = {
+        user: nameValue,
+        score: scoreValue
+    };
+    let sc = await sendData(data);
+    console.log(sc);
+    });
+  
+    render();
+  });
